ea7f5f87bdfa1ff04b4748e483e573ac0454b3db
Santiago <santimencomenco@gmail.com>
2023-07-27 16:55:12 -0500

    se agrega react

--- src/App.js
+++ src/App.js
@@ -5,18 +5,62 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div classNameName='container'>
+          <div classNameName="row">
+            <div classNameName="col-md-6">
+            <div className="alert alert-primary" role="alert">
+                A simple primary alert—check it out!
+              </div>
+              <div className="alert alert-secondary" role="alert">
+                A simple secondary alert—check it out!
+              </div>
+              <div className="alert alert-success" role="alert">
+                A simple success alert—check it out!
+              </div>
+              <div className="alert alert-danger" role="alert">
+                A simple danger alert—check it out!
+              </div>
+              <div className="alert alert-warning" role="alert">
+                A simple warning alert—check it out!
+              </div>
+              <div className="alert alert-info" role="alert">
+                A simple info alert—check it out!
+              </div>
+              <div className="alert alert-light" role="alert">
+                A simple light alert—check it out!
+              </div>
+              <div className="alert alert-dark" role="alert">
+                A simple dark alert—check it out!
+              </div>
+            </div>
+            <div className="col-md-6">
+            <div className="alert alert-primary" role="alert">
+                      A simple primary alert—check it out!
+                    </div>
+                    <div className="alert alert-secondary" role="alert">
+                      A simple secondary alert—check it out!
+                    </div>
+                    <div className="alert alert-success" role="alert">
+                      A simple success alert—check it out!
+                    </div>
+                    <div className="alert alert-danger" role="alert">
+                      A simple danger alert—check it out!
+                    </div>
+                    <div className="alert alert-warning" role="alert">
+                      A simple warning alert—check it out!
+                    </div>
+                    <div className="alert alert-info" role="alert">
+                      A simple info alert—check it out!
+                    </div>
+                    <div className="alert alert-light" role="alert">
+                      A simple light alert—check it out!
+                    </div>
+                    <div className="alert alert-dark" role="alert">
+                      A simple dark alert—check it out!
+                    </div>
+            </div>
+          </div>
+        </div>
       </header>
     </div>
   );
